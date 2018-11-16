2ef5b48f097f2e60828ec5fce365d15ba06a312a
Julian Bright <brightsparc@gmail.com>
2018-11-16 13:42:06 +1100

    Update endpoint in test.py

--- Source/test.py
+++ Source/test.py
@@ -13,13 +13,9 @@ configuration_file = sys.argv[2]
 with open(configuration_file) as f:
     data = json.load(f)
 
-commit_id = data["Parameters"]["CommitID"]
-timestamp = data["Parameters"]["Timestamp"]
+stack_name = data["Parameters"]["ParentStackName"]
 
-
-endpoint_name = endpoint_name + "-" + commit_id + "-" + timestamp
-
-runtime = boto3.client('runtime.sagemaker') 
+runtime = boto3.client('runtime.sagemaker')
 
 wget.download("http://www.vision.caltech.edu/Image_Datasets/Caltech256/images/008.bathtub/008_0007.jpg", "test.jpg")
 
@@ -27,8 +23,8 @@ wget.download("http://www.vision.caltech.edu/Image_Datasets/Caltech256/images/00
 with open("test.jpg", 'rb') as f:
     payload = f.read()
     payload = bytearray(payload)
-response = runtime.invoke_endpoint(EndpointName=endpoint_name, 
-                                   ContentType='application/x-image', 
+response = runtime.invoke_endpoint(EndpointName=endpoint_name,
+                                   ContentType='application/x-image',
                                    Body=payload)
 result = response['Body'].read()
 # result will be in json format and convert it to ndarray
